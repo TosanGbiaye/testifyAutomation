1509505639fba1046c22330f83abfecf81252c97
TosanGbiaye <36245819+TosanGbiaye@users.noreply.github.com>
2022-10-15 19:01:54 +0100

    run the code

---
+++ .github/workflows/jsCodingChallenge/challenge1.js
@@ -0,0 +1,7 @@
+//Calculate the sum of numbers within an array
+const numbers = [2,4,6,8,10]
+let sum = 0
+for (let i = 0; i < numbers.length; i++) {
+    sum  = sum + numbers[i]  
+}  
+console.log(sum)
